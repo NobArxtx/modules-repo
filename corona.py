5f269363cdcbc1b4b0eb8d64a12da0ef2879196f
NobArxtx <54905770+NobArxtx@users.noreply.github.com>
2020-03-25 13:15:13 +0530

    Update corona.py

--- corona.py
+++ corona.py
@@ -10,11 +10,11 @@ async def _(event):
     country = event.pattern_match.group(1)
     country_data = get_country_data(country, data)
     confirmed_info = country_data["confirmed"]
-Active_info = country_data["active"]
-deaths_info = country_data["deaths"]
-recovered_info = country_data["recovered"]
-country_name = country_data["country"]
-await event.edit("**Country**","**"+country_name+"**","\n**Confirmed**:",confirmed_info,"\n**Active**:",Active_info,"\n**Deaths**:",deaths_info,"\n**Recovered**:",recovered_info)
+    Active_info = country_data["active"]
+    deaths_info = country_data["deaths"]
+    recovered_info = country_data["recovered"]
+    country_name = country_data["country"]
+    await event.edit("**Country**","**"+country_name+"**","\n**Confirmed**:",confirmed_info,"\n**Active**:",Active_info,"\n**Deaths**:",deaths_info,"\n**Recovered**:",recovered_info)
 def get_country_data(country, world):
     for country_data in world:
         if country_data["country"] == country:
